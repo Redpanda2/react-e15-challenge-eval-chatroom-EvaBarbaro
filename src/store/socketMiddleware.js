f2f1188a34d5de5148182deffc6101d06f1dabc6
evabarbaro <barbaro.eva@outlook.fr>
2019-07-15 23:58:33 +0200

    Middleware try

--- src/store/socketMiddleware.js
+++ src/store/socketMiddleware.js
@@ -4,16 +4,17 @@ const socketMiddleware = store => next => (action) => {
   switch (action.type) {
     case WEBSOCKET_CONNECT: {
       const socket = window.io('http://localhost:3001');
-      //   socket.on(ADD_MESSAGE, (message) => {
-      //     store.dispatch(receiveMessage(message));
-      //   });
+      socket.on(ADD_MESSAGE, (message) => {
+        store.dispatch(receiveMessage(message));
+      });
       break;
     }
-    // case ADD_MESSAGE: {
-    //   const socket = window.io('http://localhost:3001');
-    //   socket.emit(ADD_MESSAGE, store.state.messageValue);
-    //   break;
-    // }
+    case ADD_MESSAGE: {
+      const socket = window.io('http://localhost:3001');
+      socket.emit(ADD_MESSAGE, store.getState());
+      next(action);
+      break;
+    }
     default:
       next(action);
   }
